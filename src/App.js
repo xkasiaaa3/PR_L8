0fbf7f6c1bf767e793659eccb56e7c0158afe2ec
Kasia Bączek <kasiaaa3@interia.pl>
2022-04-29 20:54:15 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import QuizComponent from "./components/QuizComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <QuizComponent/>
+    );
+}
+
+export default App;
---
+++ src/components/table/table.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import TableRow from "./table-row";
+
+const Table = (props) => {
+
+    return (
+        <div>
+            <ul className="list-group">
+                {props.answers.map((elem, key) => {
+                    return <TableRow
+                        checkAnswer={props.checkAnswer}
+                        key={key}
+                        text={elem}
+                        currentKey={key}
+                        markedAnswer={props.markedAnswer}/>
+                })}
+            </ul>
+
+
+        </div>
+    )
+};
+
+export default Table;
